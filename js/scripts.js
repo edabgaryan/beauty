84279babe579395945e123a40dee7fe8a14a9476
Edgar0995 <edgar.abgaryan15@gmail.com>
2022-11-15 17:52:26 +0200

    correction #2 15.11.2022

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
-$(document).ready(function() {
-    $('.header__open-menu').click(function(event) {
-        $('.header__open-menu, .header__menu').toggleClass('active');
-    });
-});
+const selectors = {
+    openMenu: '.header__open-menu',
+    headerMenu: '.header__menu'
+}
+const openMenu = document.querySelector(selectors.openMenu);
+const headerMenu = document.querySelector(selectors.headerMenu);
+
+
+openMenu.addEventListener('click', () => {
+    openMenu.classList.toggle('active')
+    headerMenu.classList.toggle('active')
+  });
