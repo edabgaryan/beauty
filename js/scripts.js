73ea22831f59f9607a8db78b4cc1fa12d8e0fe4f
Edgar0995 <edgar.abgaryan15@gmail.com>
2022-11-19 18:48:26 +0200

    create #4 19.11.2022

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,72 @@
 const selectors = {
     openMenu: '.header__open-menu',
-    headerMenu: '.header__menu'
+    headerMenu: '.header__menu',
+    slider: '.slider',
+    btnNext: '.btnNext',
+    btnPrev: '.btnPrev'
 }
+//openMenu
 const openMenu = document.querySelector(selectors.openMenu);
 const headerMenu = document.querySelector(selectors.headerMenu);
-
-
+//openMenu 
 openMenu.addEventListener('click', () => {
     openMenu.classList.toggle('active')
     headerMenu.classList.toggle('active')
-  });
+});
+//
 
-  initImg('#test img', [
-    'https://bs107.ru/assets/resize/7/a/7a9268ca_aa3b29fd.png', 
-    'https://bs107.ru/assets/resize/c/4/c47d2b20_aa3b29fd.png', 
-    'https://bs107.ru/assets/resize/f/5/f5d29d6a_aa3b29fd.png', 
-    'https://bs107.ru/assets/resize/4/7/47f2417a_aa3b29fd.png', 
-    'https://bs107.ru/assets/resize/b/2/b272e7ba_aa3b29fd.png'
-  ]); 
+// slider
+const slider = document.querySelector(selectors.slider);
+const sliderItems = Array.from(slider.children);
+const btnNext = document.querySelector(selectors.btnNext);
+const btnPrev = document.querySelector(selectors.btnPrev);
+
+sliderItems.forEach(function (slide, index) {
+  // чтобы только 1 слайд показывал 
+  if (index !==0) slide.classList.add('hidden'); // у hidden display: none; прописан
   
-  function initImg(selector, srcArr) {
-    const img = document.querySelector(selector); 
-    Object.assign(img, {
-      buf: Object.assign(new Image(), { img }), 
-      srcArr: [...srcArr], 
-      changeInterval: 5e3,
-      bufIdx: 0,
-      change: function () {
-        this.style.animationName = 'img-in'; 
-        this.src = this.buf.src || this.nextImage(); 
-        this.buf.src = this.nextImage(); 
-      }, 
-      nextImage: function () {
-        this.bufIdx = ++this.bufIdx < this.srcArr.length ? this.bufIdx : 0;
-        return this.srcArr[this.bufIdx];
-      }
-    }); 
-    img.buf.addEventListener('load', loadHandler); 
-    img.addEventListener('animationend', animEndHandler); 
-    img.change(); 
-    return img; 
+  // Добавляем индексы для слайдов
+  slide.dataset.index = index;
+
+  // Добавляем data атрибут active для первого / активного слайда
+  sliderItems[0].setAttribute('data-active', '')
+
+  // клик по картинке и тд.
+   /*
+  slide.addEventListener('click', function () {
+    showNextSlide('next');
+  });
+  */
+});
+
+// btn Next
+btnNext.onclick = function () {
+  showNextSlide('next');
+}
+
+// btn Prev
+btnPrev.onclick = function () {
+  showNextSlide('prev');
+}
+
+function showNextSlide(direction) {
+  // скрываем тек. слайд
+  const currentSlide = slider.querySelector('[data-active]');
+  const currentSlideIndex = +currentSlide.dataset.index;
   
-    function loadHandler() {
-      setTimeout(
-        () => this.img.style.animationName = 'img-out', 
-        this.img.changeInterval 
-      ); 
-    }
-    function animEndHandler({ animationName }) {
-      if (animationName === 'img-out') 
-        this.change(); 
-    }
+  currentSlide.classList.add('hidden');
+  currentSlide.removeAttribute('data-active');
+
+  // расчитываем след. индекс в зависимости от направления движ.
+  let nextSlideIndex;   
+  if (direction === 'next') {
+    nextSlideIndex = currentSlideIndex + 1 === sliderItems.length ? 0 : currentSlideIndex + 1;
   }
+  else if (direction === 'prev') {
+    nextSlideIndex = currentSlideIndex === 0 ? sliderItems.length - 1 : currentSlideIndex - 1;
+  }
+  // Показываем след. слайд
+  const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+  nextSlide.classList.remove('hidden');
+  nextSlide.setAttribute('data-active', '');
+};
